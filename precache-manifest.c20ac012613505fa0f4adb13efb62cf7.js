c78eedc294884615f74ee6beea6cc4a71d84676e
Rishabh Kumar <rsbkme@gmail.com>
2020-04-14 20:00:20 +0530

    Updates

---
+++ precache-manifest.c20ac012613505fa0f4adb13efb62cf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "218e3650a9f55658d553ebbdbec2a14b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bf8f171a07ab475e4ce",
+    "url": "/static/css/main.13b3aa0f.chunk.css"
+  },
+  {
+    "revision": "aedf729a6b05325b32bf",
+    "url": "/static/js/2.e3217b18.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e3217b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bf8f171a07ab475e4ce",
+    "url": "/static/js/main.c5edb969.chunk.js"
+  },
+  {
+    "revision": "64703a15ab18469962d9",
+    "url": "/static/js/runtime-main.1ae1be18.js"
+  }
+]);
